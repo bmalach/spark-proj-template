6874f5669787c0a9787031c9c03482854245dbfd
Bartek Małachowski <bartek.malachowski@gmail.com>
2020-11-20 00:57:34 +0100

    poprawki

--- src/main/java/sparkdemo/SparkDemo.java
+++ src/main/java/sparkdemo/SparkDemo.java
@@ -14,10 +14,16 @@ public class SparkDemo {
 	public static void main(String[] args) {
 		SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-//		JavaRDD<String> textFile = sc.textFile("file:///home/maria_dev/shakespeare.txt"); // ścieżka bezwzględna
+//		JavaRDD<String> textFile = sc.textFile("file:///home/user/shakespeare.txt"); // ścieżka bezwzględna
 		JavaRDD<String> textFile = sc.textFile("shakespeare.txt"); // ścieżka względna
-		JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
-				.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
+
+		JavaRDD<String> words = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator());
+		JavaPairRDD<String, Integer> counts = words.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
+
+//		zwięzły zapis łączący obie operacje
+//		JavaPairRDD<String, Integer> counts = textFile.flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
+//				.mapToPair(word -> new Tuple2<>(word, 1)).reduceByKey((a, b) -> a + b);
+
 		counts.foreach(p -> System.out.println(p));
 		System.out.println("Total words: " + counts.count());
 		sc.stop();
